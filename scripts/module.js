1d39760a195e1faa8a238ec0136ba0b81e765e81
cadowtin <chasdowtin@gmail.com>
2024-02-24 23:29:06 -0600

    Release

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,39 @@
-Hooks.once('init', async function() {
-
+Hooks.once('ready', function () {
+    Hooks.on('modifiersMatter', (data) => {
+        //console.log({ modifiers: data })
+        if (!game.settings.get("pf2e-rules-lawyer", "enabled")) return;
+        const sigMods = ["ESSENTIAL", "HELPFUL", "HARMFUL", "DETRIMENTAL"];
+        if (data?.significantModifiers.some(mod => sigMods.includes(mod.significance))) {
+            const chatWidth = chat.offsetWidth;
+            const position = "bot-right";
+            const anchor = getAnchor(position);
+            const file = 'modules/pf2e-rules-lawyer/resources/every-plus-one-extra.webm';
+            const xOffset = 40;
+            const yOffset = 0
+            const duration = 5000;
+            new Sequence()
+                .effect()
+                .file(file)
+                .screenSpace()
+                .screenSpaceAnchor(anchor)
+                .screenSpacePosition({ x: chatWidth + xOffset, y: yOffset })
+                .duration(duration)
+        }
+    })
 });
 
-Hooks.once('ready', async function() {
 
-});
+function getAnchor(position) {
+    switch (position) {
+        case 'bot-left':
+            return { x: 0, y: 1 }
+        case 'bot-right':
+            return { x: 1, y: 1 }
+        case 'top-left':
+            return { x: 0, y: 0 }
+        case 'top-right':
+            return { x: 1, y: 0 }
+        default:
+            return { x: 0.5, y: 1 }
+    }
+}
---
+++ scripts/settings.js
@@ -0,0 +1,27 @@
+Hooks.on("init", () => {
+    const debouncedReload = foundry.utils.debounce(() => window.location.reload(), 100);
+
+    game.settings.register("pf2e-rules-lawyer", "enabled", {
+        name: game.i18n.localize("pf2e-rules-lawyer.module-settings.enabled.name"),
+        hint: game.i18n.localize("pf2e-rules-lawyer.module-settings.enabled.hint"),
+        scope: "world",
+        config: true,
+        default: true,
+        type: Boolean,
+    });
+    
+    // game.settings.register("pf2e-rules-lawyer", "position", {
+    //     name: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.name"),
+    //     hint: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.hint"),
+    //     scope: "world",
+    //     config: true,
+    //     default: "roll",
+    //     type: String,
+    //     choices: {
+    //         ["top-left"]: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.choices.top-left"),
+    //         ["top-right"]: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.choices.top-right"),
+    //         ["bot-left"]: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.choices.bot-left"),
+    //         ["bot-right"]: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.choices.bot-right"),
+    //     }
+    // });
+})
