7862e91866235c4c09ff4697a0efec8e1e6816fd
cadowtin <chasdowtin@gmail.com>
2024-02-29 22:57:38 -0600

    Fixed some settings Descriptions

--- scripts/settings.js
+++ scripts/settings.js
@@ -26,6 +26,21 @@ Hooks.on("init", () => {
             ["alt-sound-image"]: game.i18n.localize("pf2e-rules-lawyer.module-settings.harmful-options.choices.alt-sound-image"),
         }
     });
+    
+    game.settings.register("pf2e-rules-lawyer", "position", {
+        name: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.name"),
+        hint: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.hint"),
+        scope: "world",
+        config: true,
+        default: "bot-right",
+        type: String,
+        choices: {
+            ["top-left"]: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.choices.top-left"),
+            ["top-right"]: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.choices.top-right"),
+            ["bot-left"]: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.choices.bot-left"),
+            ["bot-right"]: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.choices.bot-right"),
+        }
+    });
 
     game.settings.register("pf2e-rules-lawyer", "offset.x", {
         name: game.i18n.localize("pf2e-rules-lawyer.module-settings.offset.x.name"),
@@ -61,21 +76,6 @@ Hooks.on("init", () => {
         config: true,
         default: 0,
         type: Number,
-    });
-    
-    game.settings.register("pf2e-rules-lawyer", "position", {
-        name: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.name"),
-        hint: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.hint"),
-        scope: "world",
-        config: true,
-        default: "bot-right",
-        type: String,
-        choices: {
-            ["top-left"]: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.choices.top-left"),
-            ["top-right"]: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.choices.top-right"),
-            ["bot-left"]: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.choices.bot-left"),
-            ["bot-right"]: game.i18n.localize("pf2e-rules-lawyer.module-settings.position.choices.bot-right"),
-        }
     });
   
     game.settings.register("pf2e-rules-lawyer", "scale", {
