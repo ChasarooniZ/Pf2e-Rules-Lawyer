160111e73a6566ad866af94ce4ec5517fc1eac37
cadowtin <chasdowtin@gmail.com>
2024-04-02 14:08:40 -0500

    Test to fix const

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,4 +1,4 @@
 export const RULES_LAWYER_SFX = "modules/pf2e-rules-lawyer/resources/evil-rules-lawyer-sfx.ogg";
 export const RULES_LAWYER_EVIL_SFX = "modules/pf2e-rules-lawyer/resources/evil-rules-lawyer-sfx-reverb.ogg";
-export const RULES_LAWYER_VID = "modules/pf2e-rules-lawyer/resources/+1matters.webm";
-export const RULES_LAWYER_EVIL_VID = "modules/pf2e-rules-lawyer/resources/+1matters-evil.webm";
+export const RULES_LAWYER_VID = "modules/pf2e-rules-lawyer/resources/%2B1matters.webm";
+export const RULES_LAWYER_EVIL_VID = "modules/pf2e-rules-lawyer/resources/%2B1matters-evil.webm";
